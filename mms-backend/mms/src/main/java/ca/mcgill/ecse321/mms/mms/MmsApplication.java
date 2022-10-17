c63f50c78b7a6400ed6d9b7e20590aaf8d0908b3
Chrisovalantis Vatos <chrisovalantis.vatos@mail.mcgill.ca>
2022-10-17 14:54:36 -0400

    Initializing Spring Project

---
+++ mms-backend/mms/src/main/java/ca/mcgill/ecse321/mms/mms/MmsApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.mms.mms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmsApplication.class, args);
+	}
+
+}
