34786109163aec491bc141b7d867241949eb507d
sarahz2 <sarah.zhang2@mail.mcgill.ca>
2022-10-24 21:29:11 -0400

    Setup RepositoryTests classes

--- mms-backend/src/test/java/ca/mcgill/ecse321/mms/repository/ArtworkRepositoryTests.java
+++ mms-backend/src/test/java/ca/mcgill/ecse321/mms/repository/ArtworkRepositoryTests.java
@@ -1,6 +1,7 @@
 package ca.mcgill.ecse321.mms.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
