79c56246d62a0f1457de5704bc51efd07e415d92
Jean Kaznji <Jean.kaznji@mail.mcgill.ca>
2022-11-30 12:34:08 -0500

    Manage_employee_frontend

--- mms-frontend/src/components/Hello.vue
+++ mms-frontend/src/components/Hello.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="background">
     <div class="jumbotron">
-      <h1 class="display-4">Welcome to <b class = "museum-name">Marwans' Museum</b></h1>
-      <p class="lead">There are plenty of good reasons to visit Marwan's Museum! With so many artworks spanning so many different periods, plan your visit in advance to make the most of your experience. Why not pick one of our visitor trails to follow a theme-based tour? </p>
-        <span class = "museum-info"><p> We are open daily from 9:00 AM to 9:00 PM</p>   <p>|</p>      <p>Tickets Price = 25$</p></span>
+      <h1 class="display-4">Welcome to <b class="museum-name">Marwans' Museum</b></h1>
+      <p class="lead">There are plenty of good reasons to visit Marwan's Museum! With so many artworks spanning so many
+        different periods, plan your visit in advance to make the most of your experience. Why not pick one of our
+        visitor trails to follow a theme-based tour? </p>
+      <span class="museum-info">
+        <p> We are open daily from 9:00 AM to 9:00 PM</p>
+        <p>|</p>
+        <p>Tickets Price = 25$</p>
+      </span>
     </div>
   </div>
 </template>
@@ -20,35 +26,35 @@
 }
 
 .jumbotron {
-    width: 60%;
-    height: 38%;
-    margin: auto;
-    position: relative;
-    top: 180px;
+  width: 60%;
+  height: 38%;
+  margin: auto;
+  position: relative;
+  top: 180px;
 }
 
 .museum-name {
-    text-decoration: underline;
-    font-style: italic;
+  text-decoration: underline;
+  font-style: italic;
 }
 
 .lead {
-    padding-top: 1rem;
+  padding-top: 1rem;
 }
 
-.museum-info  {
-    padding-top: 2rem;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
+.museum-info {
+  margin-top: 5rem;
+  padding: 10px 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  color: black;
+  background-color: rgb(128, 128, 128, 0.7);
 }
 
-.museum-info p  {
-    margin-right: 10px;
-    font-weight: bold;
+.museum-info p {
+  margin-right: 10px;
+  font-weight: bold;
 }
-
-
-
-
 </style>
---
+++ mms-frontend/src/components/ManageEmployees.vue
@@ -0,0 +1,130 @@
+<template>
+    <div id="Manage-Employee-body">
+        <h2 id="employee-header">Employee List</h2>
+        <table id="employee-table">
+            <tr class="top-row">
+                <th>Id</th>
+                <th>Email</th>
+                <th>Username</th>
+                <th>Password</th>
+                <th>Add/Delete</th>
+            </tr>
+            <tr v-for="e in employees" :key="e.staffMemberId">
+                <td>{{ e.staffMemberID }}</td>
+                <td contenteditable="true">{{ e.email }}</td>
+                <td contenteditable="true">{{ e.username }}</td>
+                <td contenteditable="true">{{ e.password }}</td>
+                <td><button class="add-delete-employee-button">Delete</button></td>
+            </tr>
+            <tr id="last-row">
+                <td></td>
+                <td><input type="text" placeholder="Email" v-model="newEventDate"></td>
+                <td><input type="text" placeholder="Username" v-model="newEventStartTime"></td>
+                <td><input type="text" placeholder="Password" v-model="newEventEndTime"></td>
+                <td><button class="add-delete-employee-button">Add</button></td>
+            </tr>
+        </table>
+        <div id="save-discard-layout">
+            <button id="save-employee-changes-button">Save Changes</button>
+            <button id="cancel-employee-changes-button">Cancel</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+const config = require('../../config')
+
+const frontendUrl = config.dev.host + ':' + config.dev.port;
+var backendUrl = "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
+
+var AXIOS = axios.create({
+    baseURL: backendUrl,
+    headers: { "Access-Control-Allow-Origin": frontendUrl }
+});
+
+export default {
+    name: "Employee",
+    data() {
+        return {
+            employees: [],
+            id: "",
+            username: "",
+            email: "",
+            password: "",
+            successfulMessage: "",
+            failiureMessage: ""
+        };
+    },
+    created() {
+        AXIOS.get('/staffMembers')
+            .then(response => {
+                console.log(response)
+                this.employees = response.data
+            })
+            .catch(e => {
+                console.log('Error in GET /StaffMembers:')
+                console.log(e)
+            })
+    }
+}
+</script>
+
+<style>
+#Manage-Employee-body {
+    display: flex;
+    flex-direction: column;
+
+}
+
+table,
+th,
+td {
+    border: 2px solid black;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin: auto;
+    padding: 20px;
+}
+
+#employee-header {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    text-align: center;
+    margin: auto;
+}
+
+.top-row {
+    background-color: gray;
+}
+
+.add-delete-employee-button:hover,
+#cancel-employee-changes-button:hover {
+    background-color: lightgray;
+}
+
+#save-discard-layout {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    margin: auto;
+    padding-top: 20px;
+}
+
+#save-employee-changes-button {
+    background-color: green;
+    color: white;
+    margin: 25px;
+}
+
+#save-employee-changes-button:hover {
+    background-color: rgb(0, 87, 0);
+}
+
+#cancel-employee-changes-button {
+    background-color: white;
+    color: red;
+    margin: 25px;
+}
+</style>
