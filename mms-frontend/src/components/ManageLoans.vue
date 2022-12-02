369345807fb3f3d0f12601e74b89c4827e887cd1
jastaggart <jasmine.taggart@mail.mcgill.ca>
2022-12-02 04:48:05 -0500

    Modified Loans pages

--- mms-frontend/src/components/ManageLoans.vue
+++ mms-frontend/src/components/ManageLoans.vue
@@ -303,7 +303,6 @@ select {
     outline: none;
     -webkit-appearance: button;
     appearance: button;
-    outline: none;
     cursor: pointer;
     background: url(data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0Ljk1IDEwIj48ZGVmcz48c3R5bGU+LmNscy0ye2ZpbGw6IzQ0NDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPmFycm93czwvdGl0bGU+PHBvbHlnb24gY2xhc3M9ImNscy0yIiBwb2ludHM9IjEuNDEgNC42NyAyLjQ4IDMuMTggMy41NCA0LjY3IDEuNDEgNC42NyIvPjxwb2x5Z29uIGNsYXNzPSJjbHMtMiIgcG9pbnRzPSIzLjU0IDUuMzMgMi40OCA2LjgyIDEuNDEgNS4zMyAzLjU0IDUuMzMiLz48L3N2Zz4=) no-repeat 95% 50%;
     background-color: hsl(189, 55%, 52%);
--- mms-frontend/src/components/VisitorLoans.vue
+++ mms-frontend/src/components/VisitorLoans.vue
@@ -4,28 +4,45 @@
         <div>
             <p v-if="this.failureMessage" style="text-align:center; padding-top: 20px;">
                 <b>{{ this.failureMessage }}</b> <br>
-                Browse all of our loanable artworks <a href = "http://127.0.0.1:8087/?#/VisitorArtworks" >here</a>! 
+                Browse all of our loanable artworks <a href="http://127.0.0.1:8087/?#/VisitorArtworks">here</a>!
             </p>
-            <table v-else>
-                <tr>
-                    <th>Artwork</th>
-                    <th>Loan Fee</th>
-                    <th>Start Date</th>
-                    <th>End Date</th>
-                    <th>Status</th>
-                </tr>
-                <tr v-for="loan in Loans">
-                    <td>{{ loan.artwork.name }}</td>
-                    <td>{{ loan.loanFee }}</td>
-                    <td>{{ loan.startDate.substring(0, 10) }}</td>
-                    <td>{{ loan.endDate.substring(0, 10) }}</td>
-                    <td>{{ getStatus(loan) }}</td>
-                </tr>
-            </table>
-            <p>
-                Thank you for your interest in loaning our museum's artworks! <br>
-                To submit another loan request, navigate to our Artworks page <a href = "http://127.0.0.1:8087/?#/VisitorArtworks" >here</a>. 
-            </p>
+
+            <div v-else>
+
+                <p>
+                    Thank you for your interest in loaning our museum's artworks! <br>
+                    To submit another loan request, navigate to our Artwork Collection <a
+                        href="http://127.0.0.1:8087/?#/VisitorArtworks">here</a>.
+                </p>
+                <select id="statusOptions" @change.prevent="filterLoansByStatus()">
+                    <option value="Filter" selected>--Filter By Status--</option>
+                    <option value="Pending">Pending</option>
+                    <option value="Accepted/Rejected">Accepted/Rejected</option>
+                    <option value="Completed">Completed</option>
+                </select>
+
+                <!-- <p v-if="this.failureMessage1" style="color: red">
+                    {{ this.failiureMessage1 }}
+                </p> -->
+
+                <table id="myLoans">
+                    <tr>
+                        <th>Artwork</th>
+                        <th>Loan Fee</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Status</th>
+                    </tr>
+                    <tr v-for="loan in Loans">
+                        <td>{{ loan.artwork.name }}</td>
+                        <td>{{ loan.loanFee }}</td>
+                        <td>{{ loan.startDate.substring(0, 10) }}</td>
+                        <td>{{ loan.endDate.substring(0, 10) }}</td>
+                        <td>{{ getStatus(loan) }}</td>
+                    </tr>
+                </table>
+                <p ><br><b>{{this.noneFoundMessage}}</b></p>
+            </div>
         </div>
     </div>
 </template>
@@ -53,7 +70,8 @@ export default {
             isApproved: "",
             loanStatus: "",
             loanApprover: "",
-            failureMessage: ""
+            failureMessage: "",
+            noneFoundMessage: ""
         };
     },
     created: function () {
@@ -68,7 +86,9 @@ export default {
             });
     },
     methods: {
-        getStatus: function(loan) {
+        getStatus: function (loan) {
+            var currentDate = new Date();
+            var loanEndDate = new Date(loan.endDate.substring(0, 10));
             if (loan.isApproved) {
                 if (currentDate > loanEndDate) {
                     loan.loanStatus = "Completed";
@@ -78,29 +98,61 @@ export default {
             else if (loan.loanApprover == null) {
                 loan.loanStatus = "Pending"
             }
-            else /*if (loan.loanApprover != null && !loan.isApproved)*/{
+            else /*if (loan.loanApprover != null && !loan.isApproved)*/ {
                 loan.loanStatus = "Rejected"
             }
             return loan.loanStatus
+        },
+        filterLoansByStatus: function () {
+            var rowStatus, row;
+            var table = document.getElementById("myLoans")
+            var rows = table.getElementsByTagName("tr");
+            var selectedStatus = document.getElementById("statusOptions").value;
+            console.log(selectedStatus);
+            var count = 0;
+            for (let i = 0; i < rows.length; i++) {
+                row = rows[i];
+                rowStatus = row.getElementsByTagName("td")[4];
+                console.log(rowStatus);
+                if (rowStatus) {
+                    var value = rowStatus.textContent;
+                    console.log(value);
+                    if (selectedStatus == "Filter") {
+                        rows[i].style.display = "";
+                    }
+                    else if (value == selectedStatus || selectedStatus.includes(value)) {
+                        rows[i].style.display = "";
+                        count += 1;
+                    }
+                    else rows[i].style.display = "none";
+                }
+            }
+            console.log(count);
+            if (count == 0) {
+                this.noneFoundMessage = "No " + selectedStatus + " loans found.";
+            }
+            else this.noneFoundMessage = "";
         }
     }
 }   
 </script>
 
 <style scoped>
-
 h1 {
     text-align: center;
     padding: 2rem;
 }
-a{
+
+a {
     color: hsl(189, 53%, 44%);
 }
+
 p {
-    text-align:center; 
-    padding-top: 40px;
-    font-size: 1.1rem;
+    text-align: center;
+    /* padding-top: 40px; */
+    font-size: 1.2rem;
 }
+
 table {
     width: 50%;
     position: relative;
@@ -108,24 +160,54 @@ table {
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
     padding-top: 2rem;
 }
+
 th {
     background-color: hsl(189, 53%, 47%);
     color: white;
 }
-td,th {
+
+td,
+th {
     border: 1px solid #dddddd;
     text-align: center;
     padding: 15px;
     font-size: 1rem;
 }
+
 tr {
     border-bottom: 1px solid #dddddd;
 }
+
 tr:nth-of-type(even) {
     background-color: #f3f3f3;
 }
 
 tr:last-of-type {
     border-bottom: 2px solid hsl(189, 53%, 47%);
+}
+
+
+select {
+    font-size: 1.1rem;
+    width: 13rem;
+    height: 2.5rem;
+    display: flex;
+    position: relative;
+    border-radius: 4px;
+    margin: auto;
+    margin-bottom: 15px;
+    margin-top: 30px;
+    border: none;
+    outline: none;
+    cursor: pointer;
+}
+
+input {
+    width: 75%;
+    border: none;
+    box-sizing: border-box;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+    border: 1px solid black;
 }
 </style>
